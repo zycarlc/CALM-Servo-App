0690903d7d59a7cdae4249c967470c01f28b6264
leonyu <13200666@qq.com>
2023-04-03 16:10:10 +1000

    few changes

--- public/client.js
+++ public/client.js
@@ -1,5 +1,10 @@
+
+
 // Initialize and add the map
 let map;
+
+
+
 
 async function initMap() {
   // The location of Uluru
@@ -17,6 +22,30 @@ async function initMap() {
     minZoom: 10,
   });
 
+  console.log("123")
+  axios.get("/api/stations/all")
+  .then(res => console.log(res.data))
+
+
+    const infoWindow = new google.maps.InfoWindow();
+  
+    // Create the markers.
+    // tourStops.forEach((stop) => {
+    //   const marker = new google.maps.Marker({
+    //     position : stop.location,
+    //     map,
+    //     title: `${stop.name} is ${stop.distanceInKm}Km away` ,
+    //     label: `${stop.orderSeri}`,
+    //     optimized: false,
+    //   });
+  
+    //   // Add a click listener for each marker, and set up the info window.
+    //   marker.addListener("click", () => {
+    //     infoWindow.close();
+    //     infoWindow.setContent(marker.getTitle());
+    //     infoWindow.open(marker.getMap(), marker);
+    //   });
+    // });
 //   // The marker, positioned at Uluru
 //   const marker = new AdvancedMarkerView({
 //     map: map,
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const config = require("./config")
 const Servo = require("./models/servo.js")
 
 app.set("view engine", "ejs")
-
+app.use(cors())
 app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }))
 

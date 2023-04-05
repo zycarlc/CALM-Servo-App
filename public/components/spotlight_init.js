d975ebca087f97b974a2ea74b2efa26f5525b04a
Carl <zhenyuc@outlook.com>
2023-04-05 22:52:23 +1000

    keep my code and mikes

--- public/components/spotlight_init.js
+++ public/components/spotlight_init.js
@@ -11,10 +11,13 @@ export function spotlight(event){
     const spotlightLogo = document.querySelector(".spotlight-logo")
     axios.get("/api/stations/random")
         .then(res => {
+            let [ width, height ] = [ 37, 37 ]
             let spotlightOwner = res.data.station_owner
             let iconArr =["BP", "Caltex", "7-Eleven Pty Ltd", "Shell", "United"]
             if(!iconArr.includes(spotlightOwner)){
                 spotlightOwner = "default"
+                width = 26
+                height = 37
             }else if (spotlightOwner === "7-Eleven Pty Ltd"){
                 spotlightOwner = "seven11"
             }
@@ -22,7 +25,7 @@ export function spotlight(event){
             spotlightServoName.setAttribute("data-lat", res.data.latitude);
             spotlightServoName.setAttribute("data-lng", res.data.longitude);
             spotlightLogo.setAttribute("src", `../icons/100pix/${spotlightOwner}.png`)
-            spotlightLogo.setAttribute("style", "width:auto;height:30px;")
+            spotlightLogo.setAttribute("style", `width:${width}px;height:${height}px;`)
             spotlightServoName.textContent = res.data.station_name
             spotlightInfo.innerHTML = `
             <p>${res.data.station_address}, ${res.data.station_suburb}</p>`

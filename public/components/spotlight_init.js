e096ae91d50596fbbc161144a3c4616770a2f391
Carl <zhenyuc@outlook.com>
2023-04-05 22:23:36 +1000

    change markers size

--- public/components/spotlight_init.js
+++ public/components/spotlight_init.js
@@ -8,12 +8,23 @@ export function spotlight(event){
     }
     const spotlightServoName = document.querySelector(".spotlight-station-name")
     const spotlightInfo = document.getElementById("spotlight-info")
+    const spotlightLogo = document.querySelector(".spotlight-logo")
     axios.get("/api/stations/random")
         .then(res => {
+            let spotlightOwner = res.data.station_owner
+            let iconArr =["BP", "Caltex", "7-Eleven Pty Ltd", "Shell", "United"]
+            if(!iconArr.includes(spotlightOwner)){
+                spotlightOwner = "default"
+            }else if (spotlightOwner === "7-Eleven Pty Ltd"){
+                spotlightOwner = "seven11"
+            }
+
             spotlightServoName.setAttribute("data-lat", res.data.latitude);
             spotlightServoName.setAttribute("data-lng", res.data.longitude);
+            spotlightLogo.setAttribute("src", `../icons/100pix/${spotlightOwner}.png`)
+            spotlightLogo.setAttribute("style", "width:auto;height:30px;")
             spotlightServoName.textContent = res.data.station_name
             spotlightInfo.innerHTML = `
-            <p>${res.data.station_address}</p>`
+            <p>${res.data.station_address}, ${res.data.station_suburb}</p>`
         })
 }

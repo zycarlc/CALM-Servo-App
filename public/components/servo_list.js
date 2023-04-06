12904897061b5d2e315ee937318af2c94fb68c70
Michael <tharratt.michael@gmail.com>
2023-04-06 12:08:45 +1000

    fake price point API now working and outputting to nearest cards

--- public/components/servo_list.js
+++ public/components/servo_list.js
@@ -7,7 +7,6 @@ export function renderServoList (servos) {
 }
 
 function renderServo (servo) {
-    console.log(servo)
     
     let iconArr =["BP", "Caltex", "7-Eleven Pty Ltd", "Shell", "United"]
     if(!iconArr.includes(servo[1])){
@@ -24,12 +23,16 @@ function renderServo (servo) {
     <a href class="nearest-station-name" data-lat="${servo[4]}" data-lng="${servo[5]}">
         <div class="station-list" data-lat="${servo[4]}" data-lng="${servo[5]}">
             <div class="list-heading-wrapper" data-lat="${servo[4]}" data-lng="${servo[5]}">
-                <p>$${servo[5]}</p>
+                <p class="price">$${servo[6]}</p>
                 <p class="bold" data-lat="${servo[4]}" data-lng="${servo[5]}">${servo[2]}</p>  
+            </div>
+            <div class="station-card-bottom-half">
+                <div>
+                    <p class="distance" data-lat="${servo[4]}" data-lng="${servo[5]}">${servo[0]}m</p>
+                    <p class="address" data-lat="${servo[4]}" data-lng="${servo[5]}">${servo[3]}</p>
+                </div>
                 <img style="width:auto;height:30px;" src="../icons/100pix/${servo[1]}.png" data-lat="${servo[4]}" data-lng="${servo[5]}">
             </div>
-            <p class="distance" data-lat="${servo[4]}" data-lng="${servo[5]}">${servo[0]}m</p>
-            <p class="address" data-lat="${servo[4]}" data-lng="${servo[5]}">${servo[3]}</p>
         </div>
     </a>
     `
